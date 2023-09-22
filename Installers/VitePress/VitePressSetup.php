d3c3402f15a017c692a97653562cd401ee122dd8
Stephen J Carnam <steve@steveorevo.com>
2023-09-21 17:40:53 -0700

    wip

---
+++ Installers/VitePress/VitePressSetup.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hestia\WebApp\Installers\NodeBB;
+use Hestia\WebApp\Installers\BaseSetup as BaseSetup;
+require_once( '/usr/local/hestia/web/pluginable.php' );
+
+class VitePressSetup extends BaseSetup {
+	protected $appInfo = [
+		"name" => "VitePress",
+		"group" => "ssg",
+		"enabled" => true,
+		"version" => "latest",
+		"thumbnail" => "vp-thumb.png",
+	];
+ 
+	protected $appname = "vitepress";
+	protected $config = [
+		"form" => [
+			"vitepress_folder" => ["type" => "text", "value" => "", "placeholder" => "/", "label" => "Install Directory"]
+		],
+		"database" => false,
+		"resources" => [
+		],
+		"server" => [
+			"nginx" => [],
+			"php" => [
+				"supported" => ["7.3", "7.4", "8.0", "8.1", "8.2"],
+			],
+		],
+	];
+
+	public function install(array $options = null) {
+		global $hcpp;
+		$parse = explode( '/', $this->getDocRoot() );
+		$options['user'] = $parse[2];
+		$options['domain'] = $parse[4];
+		$hcpp->run( 'invoke-plugin vitepress_install ' . escapeshellarg( json_encode( $options ) ) );
+		return true;
+	}
+}
