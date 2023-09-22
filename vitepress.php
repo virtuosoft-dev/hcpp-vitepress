c983ccaa7de9a8576f1a6914a9934ed2b272f67a
Stephen J Carnam <steve@steveorevo.com>
2023-09-21 22:51:30 -0700

    wip

--- vitepress.php
+++ vitepress.php
@@ -29,7 +29,10 @@ if ( ! class_exists( 'VitePress') ) {
             $options = json_decode( $args[1], true );
             $user = $options['user'];
             $domain = $options['domain'];
+            $nodeapp_folder = "/home/$user/web/$domain/nodeapp";
             $vitepress_folder = $options['vitepress_folder'];
+            global $hcpp;
+            $vitepress_root = $hcpp->delLeftMost( $vitepress_folder, $nodeapp_folder );
 
             // Create the nodeapp folder and install vitepress
             $cmd = "mkdir -p " . escapeshellarg( $vitepress_folder ) . " && ";
@@ -38,6 +41,29 @@ if ( ! class_exists( 'VitePress') ) {
             $cmd .= 'export NVM_DIR=/opt/nvm && source /opt/nvm/nvm.sh && nvm use v18 && ';
             $cmd .= 'npm install vitepress"';
             shell_exec( $cmd );
+
+            // Copy over nodeapp files
+            $hcpp->copy_folder( __DIR__ . '/nodeapp', $vitepress_folder, $user );
+
+            // Cleanup, allocate ports, prepare nginx and start services
+            $hcpp->nodeapp->shutdown_apps( $nodeapp_folder );
+            $hcpp->nodeapp->allocate_ports( $nodeapp_folder );
+
+            // TODO: Add vitepress to nginx.conf and nginx.ssl.conf, change existing
+            // location block to include vitepress
+            // location ~ /\.(?!well-known\/|file|vitepress) {
+            //     deny all;
+            //     return 404;
+            // }
+
+            // Update proxy and restart nginx
+            if ( $nodeapp_folder . '/' == $vitepress_folder ) {
+                $hcpp->run( "change-web-domain-proxy-tpl $user $domain NodeApp" );
+            }else{
+                $hcpp->nodeapp->generate_nginx_files( $nodeapp_folder );
+                $hcpp->nodeapp->startup_apps( $nodeapp_folder );
+                $hcpp->run( "restart-proxy" );
+            }
         }
     }
     new VitePress();
