ec1f2725bb2beb8a63d9bb0ae527ff8bc66979a4
Stephen J. Carnam <steveorevo@gmail.com>
2025-03-26 16:28:55 -0700

    Update

--- plugin.php
+++ plugin.php
@@ -2,7 +2,9 @@
 /**
  * Plugin Name: VitePress
  * Plugin URI: https://github.com/virtuosoft-dev/hcpp-vitepress
- * Description: Host and maintain updated VitePress websites.
+ * Description: Host and maintain updated VitePress websites
+ * Author: Virtuosoft/Stephen J. Carnam
+ * License AGPL-3.0, for other licensing options contact support@virtuosoft.com
  */
 
 // Register the install and uninstall scripts
